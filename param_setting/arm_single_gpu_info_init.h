ba40dd32157a2d3bdbc2c9f0d8f8ecf3ebc6d49b
gxuhcj <ynszwbhcj@163.com>
2020-10-06 11:38:46 +0800

    virtual test

--- param_setting/arm_single_gpu_info_init.h
+++ param_setting/arm_single_gpu_info_init.h
@@ -22,19 +22,19 @@ class ASGInfo:public Info{
     public:
         ASGInfo();
 
-        // cl_platform_id get_platform();
-        // cl_device_id get_device();
-        // cl_context get_context();
-        // cl_command_queue get_command_queue();
+        cl_platform_id get_platform();
+        cl_device_id get_device();
+        cl_context get_context();
+        cl_command_queue get_command_queue();
 
-        // char * get_platform_name();
-        // char * get_platform_vendor();
-        // char * get_platform_version();
-        // char * get_platform_profile();
+        char * get_platform_name();
+        char * get_platform_vendor();
+        char * get_platform_version();
+        char * get_platform_profile();
 
-        // char * get_device_name();
-        // char * get_deice_vendor();
-        // long get_device_mem_size();
+        char * get_device_name();
+        char * get_deice_vendor();
+        long get_device_mem_size();
 
 };
 
